97b57322a3194cf0e04d792d0e4f24fea7b02a91
MariaAnnaVibin <mariaannavibin@gmail.com>
2026-06-15 12:26:19 +0530

    add message

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 let box = document.getElementsByTagName("button");
+let prediction = document.getElementById("prediction");
+
+let timerStarted = false;
 
 box[0].addEventListener("mouseover", function () {
     box[0].style.marginLeft = Math.floor(Math.random() * 30) + "rem";
     box[0].style.marginTop = Math.floor(Math.random() * 20) + "rem";
+
+    if (!timerStarted) {
+        timerStarted = true;
+
+        setTimeout(function () {
+            prediction.innerHTML = "Focus on the Present. The future is unpredictable.";
+            box[0].style.display = "none";
+        }, 10000);
+    }
 });
